58fe217116e6d1ff935380b50cf7145d7edd5623
yulo01 <giorgi.kuliashvili.1@btu.edu.ge>
2023-04-06 17:15:05 +0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { ServicesComponent } from './services/services.component';
+import { NeedsComponent } from './needs/needs.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    ServicesComponent,
+    NeedsComponent,
+    FooterComponent,
+   
   ],
   imports: [
     BrowserModule,
